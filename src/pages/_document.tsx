d1b1d3a96ba3d9911235b1f6d761e46c67c62f22
eliasall3x <elliasallex@gmail.com>
2021-05-14 23:14:49 -0300

    feat: styled-components configuration

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,9 @@
-import Document from "next/document"
+import React from "react"
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from "next/document"
 import { ServerStyleSheet } from "styled-components"
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
@@ -25,5 +26,24 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;1,100;1,200&family=Inter:wght@100;200;300;400;500&family=Roboto:ital,wght@0,300;0,400;0,500;1,300&display=swap" rel="stylesheet" />
+
+          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
