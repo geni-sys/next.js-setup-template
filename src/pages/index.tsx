b5b6b67aa6a31c384a20e136de02f6680d52c603
eliasall3x <elliasallex@gmail.com>
2021-05-14 21:03:23 -0300

    feat: index tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <main>
+        <h1>Hello world!</h1>
+        <h5>eliasallex</h5>
+      </main>
+    </div>
+  );
+}
