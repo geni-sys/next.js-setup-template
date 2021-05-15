3debeee999e394134106e2ad9be4f126287c9c2a
eliasall3x <elliasallex@gmail.com>
2021-05-14 22:48:33 -0300

    feat: eslint new rules

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   semi: false, // ponto e vírgula no final das linhas?
   singleCote: true, // se vai utilizar aspas simples
-  arrowParens: "avoid", // parents em arrowFunction
-  trailingComma: "none", // vírgula no final dos Obnjects
+  arrowParens: true, // parents em arrowFunction
+  trailingComma: "none", // vírgula no final dos objects
   endOfLine: "auto", // LF ou CRLF
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Head from "next/head"
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <div>
       <Head>
@@ -15,3 +15,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
