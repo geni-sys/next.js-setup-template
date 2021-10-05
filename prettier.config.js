d6d29db7d17bcd45b3f19f3af265fc1f53e4dec1
elias alexandre <elliasallex@gmail.com>
2021-10-04 21:51:06 -0300

    feat: new environment configs

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  semi: false, // ponto e vírgula no final das linhas?
-  singleCote: true, // se vai utilizar aspas simples
+  semi: true, // ponto e vírgula no final das linhas?
+  singleCote: false, // se vai utilizar aspas simples
   arrowParens: true, // parents em arrowFunction
   trailingComma: "none", // vírgula no final dos objects
   endOfLine: "auto", // LF ou CRLF
